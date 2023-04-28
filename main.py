92c062703b1c6ab57da43d80f2778759dffb31b6
Fadi Sarwat <fady.sarwat377@gmail.com>
2023-04-28 03:34:13 +0300

    Add cropped icons

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
 import pyautogui
 from screeninfo import get_monitors
+import glob
+from calculate_time import CalculateTime
 
 monitor = get_monitors()[0]
-program_icons = [
-    'someButton.png',
-]
+program_icons = glob.glob("./icons/*")
+
+pyautogui.hotkey('win', 'd')
+pyautogui.moveTo(1, 1)
+pyautogui.click()
 
 for program in program_icons:
-    position_of_program = pyautogui.locateOnScreen(program, region=(0, 0, 400, monitor.height))
-    if position_of_program is not None:
-        pyautogui.doubleClick(position_of_program)
+    position = pyautogui.locateOnScreen(
+        program,
+        confidence=0.8,
+        region=(0, 0, 600, monitor.height),
+        grayscale=True
+    )
+    if position is not None:
+        position = pyautogui.center(position)
+        calc = CalculateTime(lambda: open_program_in_location(position))
+        time = calc.execute()
+        print(program + ": " + time)
 
+
+def open_program_in_location(location):
+    pyautogui.moveTo(location)
+    pyautogui.doubleClick(location)

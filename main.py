bbaa5135d354d3fe04e4ff428247b3e686b4ce1d
Fadi Sarwat <fady.sarwat377@gmail.com>
2023-04-28 05:04:27 +0300

    Add loop 10 times

--- main.py
+++ main.py
@@ -21,24 +21,29 @@ def get_pixel_in_center():
 monitor = get_monitors()[0]
 program_icons = glob.glob("./icons/*")
 
-for program in program_icons:
-    go_to_desktop()
+for i in range(0, 10):
+    print("Iteration " + str(i+1))
+    for program in program_icons:
+        go_to_desktop()
 
-    position = pyautogui.locateOnScreen(
-        program,
-        confidence=0.6,
-        region=(0, 0, 600, monitor.height),
-        grayscale=False
-    )
-    if position is not None:
-        position = pyautogui.center(position)
-        color_before = get_pixel_in_center()
-        start = time.time_ns()
-        open_program_in_location(position)
-        while get_pixel_in_center() == color_before:
-            pass
-        end = time.time_ns()
-        pyautogui.hotkey('alt', 'f4')
-        exec_time = end - start
-        name = program.split("\\")[1].split(".")[0]
-        print(name + ": " + str(exec_time) + " ns")
+        position = pyautogui.locateOnScreen(
+            program,
+            confidence=0.65,
+            region=(0, 0, 600, monitor.height),
+            grayscale=False
+        )
+        if position is not None:
+            position = pyautogui.center(position)
+            color_before = get_pixel_in_center()
+            start = time.time_ns()
+            open_program_in_location(position)
+            while get_pixel_in_center() == color_before:
+                pass
+            end = time.time_ns()
+            pyautogui.hotkey('alt', 'f4')
+            exec_time = end - start
+            name = program.split("\\")[1].split(".")[0]
+            print(name + ": " + str(exec_time) + " ns")
+
+    print()
+    time.sleep(5)
